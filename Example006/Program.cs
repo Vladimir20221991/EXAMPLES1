f032b6691ef567180acba642eca82b0377d54d57
Vladimir20221991 <Dj_vold@mail.ru>
2022-04-28 16:07:54 +0300

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "машуля,артем")
+
+{
+    Console.WriteLine("Ура, это же Машуля!");
+    Console.WriteLine("Ура, это же крутой вратарь Артем!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
