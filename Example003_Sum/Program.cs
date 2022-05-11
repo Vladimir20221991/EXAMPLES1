1b312356be5fd0b6c1ade60992cab0196066eac7
Vladimir20221991 <Dj_vold@mail.ru>
2022-05-11 15:31:32 +0300

    Example003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,13 +1,14 @@
-/*int numberA = 80;
+﻿/*
+int numberA = 80;
 int numberB = 7;
 int result = numberA + numberB;
 Console.WriteLine(result);
 
 
-int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9 
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+// int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9 
+// Console.WriteLine(numberA);
+// int numberB = new Random().Next(1, 10);
+// Console.WriteLine(numberB);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
 */
